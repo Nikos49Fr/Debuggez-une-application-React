c72a76006b93d61798584867cc3b4ae73eac3250
Nicolas Grignard <nicolas4980@gmail.com>
2025-11-17 13:32:12 +0100

    Slider : sortie du bloc de pagination du 1er map afin qui ne soit pas répété et provoque le bug des boutons radios.

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -23,37 +23,36 @@ const Slider = () => {
   });
   return (
     <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+        {byDateDesc?.map((event, idx) => (
+            <div
+                key={event.title}
+                className={`SlideCard SlideCard--${
+                index === idx ? "display" : "hide"
+                }`}
+            >
+                <img src={event.cover} alt="forum" />
+                <div className="SlideCard__descriptionContainer">
+                <div className="SlideCard__description">
+                    <h3>{event.title}</h3>
+                    <p>{event.description}</p>
+                    <div>{getMonth(new Date(event.date))}</div>
+                </div>
+                </div>
             </div>
-          </div>
-          <div className="SlideCard__paginationContainer">
+        ))}
+        {/* Correction : Sortie du bloc de pagination du premier map pour éviter la répition et le bug du bouton radio */}
+        <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
+                {byDateDesc?.map((event, radioIdx) => (
                 <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
+                    key={event.title}
+                    type="radio"
+                    name="radio-button"
+                    checked={index === radioIdx}
                 />
-              ))}
+                ))}
             </div>
-          </div>
-        </>
-      ))}
+        </div>
     </div>
   );
 };
