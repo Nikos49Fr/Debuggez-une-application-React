cd438988008b490c0872e8424f849a68b253b34b
Nicolas Grignard <nicolas4980@gmail.com>
2025-11-17 11:42:51 +0100

    Slider : Correction du helper Date : numérotation des mois corrigée (de 0 à 11)

--- src/helpers/Date/index.js
+++ src/helpers/Date/index.js
@@ -1,16 +1,18 @@
 export const MONTHS = {
-  1: "janvier",
-  2: "février",
-  3: "mars",
-  4: "avril",
-  5: "mai",
-  6: "juin",
-  7: "juillet",
-  8: "août",
-  9: "septembre",
-  10: "octobre",
-  11: "novembre",
-  12: "décembre",
+  0: "janvier",
+  1: "février",
+  2: "mars",
+  3: "avril",
+  4: "mai",
+  5: "juin",
+  6: "juillet",
+  7: "août",
+  8: "septembre",
+  9: "octobre",
+  10: "novembre",
+  11: "décembre",
 };
+// Correction : la méthode native getMonth de la fonction date (date.getMonth) renvoi les numéros de mois de 0 à 11, et non de 1 à 12.
+// Correction de la numétation dans la constante MONTHS
 
 export const getMonth = (date) => MONTHS[date.getMonth()];
